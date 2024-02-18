afe5a99c038d54ed9c3481ae20e65b6372ba23fc
dinuwan <dinuwan@orelit.com>
2024-02-18 14:40:12 +0530

    Simple Interceptor implemented and tested

---
+++ rest-client-sample/src/main/java/org/nod/restclient/interceptors/MyInterceptor.java
@@ -0,0 +1,23 @@
+package org.nod.restclient.interceptors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class MyInterceptor implements ClientHttpRequestInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(MyInterceptor.class);
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+        log.info("Intercepting Request: "+ request.getURI());
+        request.getHeaders().add("x-request-id", "123456");
+        return execution.execute(request, body);
+    }
+}
