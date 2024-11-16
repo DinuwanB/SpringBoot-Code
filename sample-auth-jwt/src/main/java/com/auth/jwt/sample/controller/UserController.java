540b7e760881bf76cd25667e521fc76c6c360861
DinuwanB <dinubhagya97@gmail.com>
2024-11-16 20:06:44 +0530

    SecurityConfig / UserController files added with impl

---
+++ sample-auth-jwt/src/main/java/com/auth/jwt/sample/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.auth.jwt.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class UserController {
+
+    @Autowired
+    private UserInfoService userService;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/home")
+    public String welcome() {
+        return "Welcome to this public endpoint";
+    }
+
+    @PostMapping("/register")
+    public String addNewUser(@RequestBody UserInfo userInfo) {
+        return userService.saveUser(userInfo);
+    }
+
+    @GetMapping("/user/profile")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String userProfile() {
+        return "Welcome to the User Profile";
+    }
+
+    @GetMapping("/admin/profile")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminProfile() {
+        return "Welcome to the Admin Profile";
+    }
+
+    @PostMapping("/generateToken")
+    public String authenticateAndGetToken(@RequestBody AuthRequest authRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword())
+        );
+
+        if (authentication.isAuthenticated()){
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("Username not found exception");
+        }
+    }
+
+}
